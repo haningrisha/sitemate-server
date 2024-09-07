3cc7f5e1ae4fbfbeb2c4178dc1a4a14bba8ebac1
Ханин Григорий <hanin@digipax.dev>
2024-09-07 16:29:53 +0400

    Fixed routes prefix

--- routes/issue.py
+++ routes/issue.py
@@ -7,23 +7,23 @@ from crud import issue_crud
 
 
 router = APIRouter(
-    prefix='/account',
-    tags=['account'],
+    prefix='/issues',
+    tags=['issues'],
     responses={404: {'description': 'Not found'}},
 )
 
 
-@router.post("/issues/", response_model=Issue)
+@router.post("/", response_model=Issue)
 def create_issue(issue: Issue):
     return issue_crud.create_issue(issue)
 
 
-@router.get("/issues/", response_model=List[Issue])
+@router.get("/", response_model=List[Issue])
 def read_issues():
     return issue_crud.read_issues()
 
 
-@router.get("/issues/{issue_id}", response_model=Issue)
+@router.get("/{issue_id}", response_model=Issue)
 def read_issue(issue_id: UUID):
     issue = issue_crud.read_issue(issue_id)
     if issue is None:
@@ -31,15 +31,15 @@ def read_issue(issue_id: UUID):
     return issue
 
 
-@router.put("/issues/{issue_id}", response_model=Issue)
+@router.put("/{issue_id}", response_model=Issue)
 def update_issue(issue_id: UUID, updated_issue: Issue):
-    updated_issue.id = issue_crud.update_issue(issue_id, updated_issue)
+    updated_issue = issue_crud.update_issue(issue_id, updated_issue)
     if updated_issue is None:
         raise HTTPException(status_code=404, detail="Issue not found")
     return updated_issue
 
 
-@router.delete("/issues/{issue_id}")
+@router.delete("/{issue_id}")
 def delete_issue(issue_id: UUID):
     deleted_issue = issue_crud.delete_issue(issue_id)
     if deleted_issue is None:
